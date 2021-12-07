43acdd985efffb8b38f45bf1bc7fa97cc6bd96d0
MadMan <mr.mad2k@gmail.com>
2021-12-07 23:51:40 +0100

    Connect Javalin, add getApp()

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        getApp().start(7000);
+    }
+
+    private static Javalin getApp() {
+        Javalin app = Javalin.create(config -> config.enableDevLogging());
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
     }
 }
