80046250d77e9d59eff261abebbcb6626928ff23
MadMan <mr.mad2k@gmail.com>
2021-12-08 20:59:53 +0100

    checkstyle fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,9 +6,7 @@ public class App {
 //    private static final int PORT = 7000;
 
     public static void main(String[] args) {
-
         getApp().start();
-        
     }
 
     private static Javalin getApp() {
