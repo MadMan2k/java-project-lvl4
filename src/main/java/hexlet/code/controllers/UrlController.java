8070abd87f550034495ce05729ec3348ebedfbf2
MadMan <mr.mad2k@gmail.com>
2022-01-05 22:36:51 +0100

    add getNormalizedURL() in UrlController.java

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -11,6 +11,7 @@ import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Locale;
@@ -47,31 +48,6 @@ public final class UrlController {
         ctx.render("URLs/index.html");
     }
 
-//    public final static Handler listOfURLs = ctx -> {
-//        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
-//
-//        PagedList<UrlModel> pagedUrlModels = new QUrlModel()
-//                .setFirstRow(page * ROWS_PER_PAGE)
-//                .setMaxRows(ROWS_PER_PAGE)
-//                .orderBy()
-//                .id.desc()
-//                .findPagedList();
-//
-//        List<UrlModel> urlModels = pagedUrlModels.getList();
-//
-//        int lastPage = pagedUrlModels.getTotalPageCount() + 1;
-//        int currentPage = pagedUrlModels.getPageIndex() + 1;
-//        List<Integer> pages = IntStream
-//                .range(1, lastPage)
-//                .boxed()
-//                .collect(Collectors.toList());
-//
-//        ctx.attribute("urlModels", urlModels);
-//        ctx.attribute("pages", pages);
-//        ctx.attribute("currentPage", currentPage);
-//        ctx.render("URLs/index.html");
-//    };
-
     public static void getCreateURL(Context ctx) {
         String inputURL = ctx.formParam("url");
 
@@ -80,23 +56,34 @@ public final class UrlController {
             return;
         }
 
-        inputURL = inputURL.toLowerCase(Locale.ROOT);
 
-//        inputURL = UrlStandardizer.standardize(inputURL);
+        String normalizedURL = getNormalizedURL(inputURL);
 
-        try {
-            URL url = new URL(inputURL);
-//            inputURL = "https://" + url.getHost();
-            inputURL = url.getProtocol() + "://" + url.getHost();
-            if (url.getPort() != -1) {
-                inputURL = inputURL + ":" + url.getPort();
-            }
-        } catch (Exception e) {
+
+
+
+//        inputURL = inputURL.toLowerCase(Locale.ROOT);
+//
+//        try {
+//            URL url = new URL(inputURL);
+//            inputURL = url.getProtocol() + "://" + url.getHost();
+//            if (url.getPort() != -1) {
+//                inputURL = inputURL + ":" + url.getPort();
+//            }
+//        } catch (Exception e) {
+//            ctx.sessionAttribute("flash", "Invalid URL");
+//            ctx.sessionAttribute("flash-type", "danger");
+//            ctx.redirect("/");
+//            return;
+//        }
+
+        if ("Invalid URL".equals(normalizedURL) ) {
             ctx.sessionAttribute("flash", "Invalid URL");
             ctx.sessionAttribute("flash-type", "danger");
             ctx.redirect("/");
             return;
         }
+
 
         if (new QUrlModel().name.equalTo(inputURL).exists()) {
             ctx.sessionAttribute("flash", "The site already exists in the database");
@@ -104,7 +91,6 @@ public final class UrlController {
             ctx.redirect("/urls");
             return;
         }
-
 
         UrlModel urlModel = new UrlModel(inputURL);
         urlModel.save();
@@ -117,50 +103,24 @@ public final class UrlController {
         ctx.redirect("/urls");
     }
 
-//    public final static Handler createURL = ctx -> {
-//        String inputURL = ctx.formParam("url");
-//
-//        if (inputURL == null) {
-//            ctx.redirect("/");
-//            return;
-//        }
-//
-//        inputURL = inputURL.toLowerCase(Locale.ROOT);
-//
-////        inputURL = UrlStandardizer.standardize(inputURL);
-//
-//        try {
-//            URL url = new URL(inputURL);
-////            inputURL = "https://" + url.getHost();
-//            inputURL = url.getProtocol() + "://" + url.getHost();
-//            if (url.getPort() != -1) {
-//                inputURL = inputURL + ":" + url.getPort();
-//            }
-//        } catch (Exception e) {
-//            ctx.sessionAttribute("flash", "Invalid URL");
-//            ctx.sessionAttribute("flash-type", "danger");
-//            ctx.redirect("/");
-//            return;
-//        }
-//
-//        if (new QUrlModel().name.equalTo(inputURL).exists()) {
-//            ctx.sessionAttribute("flash", "The site already exists in the database");
-//            ctx.sessionAttribute("flash-type", "info");
-//            ctx.redirect("/urls");
-//            return;
-//        }
-//
-//
-//        UrlModel urlModel = new UrlModel(inputURL);
-//        urlModel.save();
-//
-//        String flashRus = " / Страница успешно добавлена";
-//
-//        ctx.sessionAttribute("flash", "The site was successfully added" + flashRus);
-//        ctx.sessionAttribute("flash-type", "success");
-//
-//        ctx.redirect("/urls");
-//    };
+    private static String getNormalizedURL(String inputURL) {
+        inputURL = inputURL.toLowerCase(Locale.ROOT);
+
+        URL url = null;
+        try {
+            url = new URL(inputURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return "Invalid URL";
+        }
+        String normalizedURL = url.getProtocol() + "://" + url.getHost();
+        if (url.getPort() != -1) {
+            normalizedURL = normalizedURL + ":" + url.getPort();
+        }
+
+        return normalizedURL;
+
+    }
 
     public static void getShowURL(Context ctx) {
         int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
@@ -176,21 +136,6 @@ public final class UrlController {
         ctx.attribute("urlModel", urlModel);
         ctx.render("URLs/show.html");
     }
-
-//    public final static Handler showURL = ctx -> {
-//        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
-//
-//        UrlModel urlModel = new QUrlModel()
-//                .id.equalTo(id)
-//                .findOne();
-//
-//        if (urlModel == null) {
-//            throw new NotFoundResponse();
-//        }
-//
-//        ctx.attribute("urlModel", urlModel);
-//        ctx.render("URLs/show.html");
-//    };
 
     public static void getCheckURL(Context ctx) {
         String title;
@@ -261,94 +206,10 @@ public final class UrlController {
 
         ctx.attribute("urlModel", urlModel);
 
-//        String consumeSessionAttribute = ctx.consumeSessionAttribute("flash");
-//        System.out.println(consumeSessionAttribute);
-
-
         String flashRus = " / Страница успешно проверена";
 
         ctx.sessionAttribute("flash", "The site was successfully checked" + flashRus);
         ctx.sessionAttribute("flash-type", "info");
         ctx.redirect("/urls/" + urlModel.getId());
     }
-
-//    public final static Handler checkURL = ctx -> {
-//        String title;
-//        String h1;
-//        String description;
-//        int statusCode;
-//        Document doc;
-//        Connection.Response response;
-//
-//        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
-//
-//        UrlModel urlModel = new QUrlModel()
-//                .id.equalTo(id)
-//                .findOne();
-//
-//        if (urlModel == null) {
-//            throw new NotFoundResponse();
-//        }
-//
-//        String urlAsString = urlModel.getName();
-//
-//        try {
-//            response = Jsoup.connect(urlAsString)
-//                    .timeout(TIMEOUT_LIMIT)
-//                    .execute();
-//            doc = response.parse();
-//            statusCode = response.statusCode();
-//        } catch (Exception e) {
-//            if (e instanceof HttpStatusException) {
-//                statusCode = ((HttpStatusException) e).getStatusCode();
-//            } else {
-//                statusCode = NOT_FOUND_CODE;
-//            }
-//
-//            UrlCheckModel urlCheckModel = new UrlCheckModel(statusCode, "", "", "");
-//            urlModel.addCheckToUrl(urlCheckModel);
-//            urlModel.save();
-//            ctx.attribute("urlModel", urlModel);
-//            ctx.sessionAttribute("flash", "Server connection timeout error. "
-//                    + "It looks like this site has been working hard and is resting now :(");
-//            ctx.sessionAttribute("flash-type", "danger");
-//            ctx.redirect("/urls/" + urlModel.getId());
-//            return;
-//        }
-//
-//        try {
-//            title = doc.title();
-//        } catch (Exception e) {
-//            title = "";
-//        }
-//
-//        try {
-//            h1 = doc.select("h1").first().text();
-//        } catch (Exception e) {
-//            h1 = "";
-//        }
-//
-//        try {
-//            description = doc.select("meta[name=description]").get(0).attr("content");
-//        } catch (Exception e) {
-//            description = "";
-//        }
-//
-//        UrlCheckModel urlCheckModel = new UrlCheckModel(statusCode, title, h1, description);
-//
-//        urlModel.addCheckToUrl(urlCheckModel);
-//        urlModel.save();
-//
-//        ctx.attribute("urlModel", urlModel);
-//
-////        String consumeSessionAttribute = ctx.consumeSessionAttribute("flash");
-////        System.out.println(consumeSessionAttribute);
-//
-//
-//        String flashRus = " / Страница успешно проверена";
-//
-//        ctx.sessionAttribute("flash", "The site was successfully checked" + flashRus);
-//        ctx.sessionAttribute("flash-type", "info");
-//        ctx.redirect("/urls/" + urlModel.getId());
-//    };
 }
